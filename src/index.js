c7aaac213c4c1762d61ceb435bb12d7fe4dcbd6b
Peter Schussheim <peter@schussheim.com>
2017-10-10 12:00:31 -0400

    add hot module replacement, base styles and colors module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './styles/base.css'
 
 const rootEl = document.getElementById('root')
 
---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  success: '#29A88E',
+  danger: '#C65F4A',
+  primary: '#6DCFD3',
+  info: '#FFD035',
+  gray: '#5A6E73',
+  accent: '#8E83A3'
+}
+
+export default colors
